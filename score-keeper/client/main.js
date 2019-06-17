f494cd0241e397af52dd115b938ec0f9f266b99d
Kevin <stradtkt22@gmail.com>
2019-06-17 09:00:56 -0400

    Added a form to add a player

--- score-keeper/client/main.js
+++ score-keeper/client/main.js
@@ -10,6 +10,18 @@ const renderPlayers = (playersList) => {
     });
 }
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    let playerName = e.target.playerName.value;
+    if(playerName) {
+        e.target.playerName.value = '';
+        Players.insert({
+            name: playerName,
+            score: 0
+        });
+    }
+}
+
 Meteor.startup(() => {
     Tracker.autorun(() => {
         let players = Players.find().fetch();
@@ -20,6 +32,10 @@ Meteor.startup(() => {
                 <h1>{title}</h1>
                 <p>Hello {name}</p>
                 {renderPlayers(players)}
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="playerName" placeholder="Player Name"/>
+                    <button type="submit">Add Player</button>
+                </form>
             </div>
         );
         ReactDOM.render(jsx, document.getElementById('app'));
