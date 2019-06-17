5fcdb9dd69b34b7b412c4bfad7e9bb51c311b7db
Kevin <stradtkt22@gmail.com>
2019-06-17 08:45:54 -0400

    Added some items

--- score-keeper/client/main.js
+++ score-keeper/client/main.js
@@ -1,22 +1,27 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+import {Tracker} from 'meteor/tracker';
 
-import './main.html';
+const renderPlayers = (playersList) => {
+    return playersList.map((player) => {
+        return <p key={player._id}>{player.name} has {player.score} points(s)</p>;
+    });
+}
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+    Tracker.autorun(() => {
+        let players = Players.find().fetch();
+        let title = 'Score Keeper';
+        let name = '';
+        let jsx = (
+            <div>
+                <h1>{title}</h1>
+                <p>Hello {name}</p>
+                {renderPlayers(players)}
+            </div>
+        );
+        ReactDOM.render(jsx, document.getElementById('app'));
+    });
 });
