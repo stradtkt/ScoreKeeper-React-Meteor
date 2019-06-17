3f75221698a2a49547b51992d2a0ad69bd7c267c
Kevin <stradtkt22@gmail.com>
2019-06-17 09:29:23 -0400

    Added inc and dec buttons

--- score-keeper/client/main.js
+++ score-keeper/client/main.js
@@ -8,6 +8,8 @@ const renderPlayers = (playersList) => {
     return playersList.map((player) => {
         return (
             <p key={player._id}>{player.name} has {player.score} points(s)
+                <button onClick={() => Players.update({_id: player._id}, {$inc:{score: -1}})}>-</button>
+                <button onClick={() => Players.update({_id: player._id}, {$inc:{score: 1}})}>+</button>
                 <button onClick={() => Players.remove({_id: player._id})}>&times;</button>
             </p>
         );
